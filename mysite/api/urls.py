64b9cb90c3898bc53d6854b37f464d6c989548b0
Alex <48474870+aon2003@users.noreply.github.com>
2022-01-05 21:14:29 +0200

    urls.py - version update fixes api_processor.py - fixed (the functions didn't really return the longest post title and comment, they actually sorted them alphabetically)

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -1,13 +1,12 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 from api import views
 from api.views import CarsView
 from rest_framework_swagger.views import get_swagger_view
-from django.conf.urls import url
 
 schema_view = get_swagger_view(title='Cars API')
 
 urlpatterns = [
-    url(r'docs/', schema_view),
+    re_path(r'docs/', schema_view),
     path('cars/get', views.cars_json),
     path('cars/create', views.create_car),
     path('view_cars/', CarsView.as_view()),
